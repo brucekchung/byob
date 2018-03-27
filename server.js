102d65548f9d5f54a225bf7d52c658f2b3907400
Jeff Bender <j25bender@gmail.com>
2018-03-27 14:14:56 -0600

    add to server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const environment = process.env.NODE_ENV || 'development'
-//const configuration = require('./knexfile')[environment]
-//const database = require('knex')(configuration)
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
+app.set('port', process.env.NODE_ENV || 3000);
+app.use(express.static('public'));
+app.use(bodyParser.json())
