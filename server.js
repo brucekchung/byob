368c29917c5f97f2e93db7853378a3879e5241f7
Jeff Bender <j25bender@gmail.com>
2018-03-29 15:48:44 -0600

    added get sad paths

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/api/v1/strengths/:id', (request, response) => {
     response.status(200).json(strengths)
   })
   .catch(error => {
-    response.status(500).json({error})
+    response.status(500).json(`Invalid Strength ID: ${error}`)
   })
 })
 
@@ -47,7 +47,7 @@ app.get('/api/v1/people/:id', (request, response) => {
     response.status(200).json(people)
   })
   .catch(error => {
-    response.status(500).json({error})
+    response.status(500).json(`Invalid People ID: ${error}`)
   })
 })
 
@@ -66,7 +66,7 @@ app.post('/api/v1/people/:people_id/strengths/', async (request, response) => {
 
 app.post('/api/v1/people/', (request, response) => {
   const { name } = request.body
-
+  
   if (!name) {
     return response
       .status(422)
@@ -74,8 +74,8 @@ app.post('/api/v1/people/', (request, response) => {
   }
 
   database('people').insert({name}, 'id')
-  .then(people => {
-    response.status(201).json({ id: request.body[0], name })
+  .then(people => {    
+    response.status(201).json({ id: people[0], name })
   })
   .catch(error => {
     response.status(500).json({error})
